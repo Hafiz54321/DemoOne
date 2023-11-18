61b5dbba90223cc66c7889cc2e3112e1cb29f53b
Hafiz54321 <banofarhan88@gmail.com>
2023-11-18 20:23:47 +0530

    First commit...

---
+++ src/com/qa/one/Subtraction.java
@@ -0,0 +1,16 @@
+package com.qa.one;
+
+public class Subtraction {
+
+	public static int a = 20;
+	public static int b = 10;
+	public static int res = 0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	res = a - b;
+	
+	System.out.println("Subtraction of a and b is: "+ res);
+		
+	}
+
+}
